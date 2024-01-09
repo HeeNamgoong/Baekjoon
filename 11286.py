476265d81896b14a063922b3dd546c5fa9f7e79a
HeeNamgoong <skarndgml2@kookmin.ac.kr>
2024-01-09 13:15:31 +0900

    1/9 절댓값 힙

---
+++ 11286.py
@@ -0,0 +1,18 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+
+N = int(input())
+
+heap = []
+for i in range(N):
+    x = int(input())
+    
+    if x != 0:
+        heapq.heappush(heap, (abs(x), x))
+    elif x == 0:
+        if len(heap) == 0:
+            print(0)
+        else:
+            print(heapq.heappop(heap)[1])
