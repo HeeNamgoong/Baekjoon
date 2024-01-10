54a7cfe6933a93e99c015c92a9537f5ec7a6fabd
HeeNamgoong <skarndgml2@kookmin.ac.kr>
2024-01-10 23:26:49 +0900

    1/10 풍선 터뜨리기

--- 2346.py
+++ 2346.py
@@ -19,3 +19,46 @@ while deq:
         deq.rotate(-num)
 
 print(*ans)
+
+
+''' 실패 코드! deque에 rotate라는 함수가 내장되어있는 걸을 몰랐음!
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+num = list(map(int, input().split()))
+num_copied = num[:]
+ans = []
+
+idx = 0
+for i in range(N):
+    
+    if num[idx] > 0:
+        idx += num[idx]
+    else:
+        idx -= num[idx]
+    ans.append(idx + 1)
+    del num_copied[idx]
+    
+    num[idx]
+
+
+print(ans)
+
+
+idx = 0
+del num_copied[0]
+
+for i in range(N-1):
+    if num[idx] > 0:
+        idx += num[idx]
+        ans.append(idx + 1)
+        del num_copied[idx - 1]
+        idx = idx - i
+    else:
+        idx -= num[idx]
+        ans.append(idx + 1 + N)
+        del num_copied[idx]
+        idx = idx
+'''
