d5b564ffa8ac33bd97c5a8148a11eb93da871032
HeeNamgoong <skarndgml2@kookmin.ac.kr>
2024-08-15 23:12:58 +0900

    회전하는 큐 - 8/15

--- 1021.py
+++ 1021.py
@@ -1,18 +1,28 @@
 import sys
-
+from collections import deque
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
-que = []
+num = list(map(int, input().split()))
 
+que = deque()
 for i in range(1, N+1):
-    if i % 3 == 1:
-        del que[0]
-    elif i % 3 == 2:
-        que.append(que[0])
-        del que[0]
-    elif i % 3 == 0:
-        que.insert(0, que[-1])
-        del que[-1]
-        
-print(que[M])
+    que.append(i)
+
+ans = 0
+for i in num:
+    while True:
+        if que[0] == i: # 1번
+            que.popleft()
+            break # 다음으로 뽑을 숫자로 넘어감
+        else:
+            if que.index(i) < len(que) / 2: # 2번
+                while que[0] != i:
+                    que.append(que.popleft())
+                    ans += 1
+            else: # 3번
+                while que[0] != i:
+                    que.appendleft(que.pop())
+                    ans += 1
+
+print(ans)
