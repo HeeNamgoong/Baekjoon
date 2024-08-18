2aa53a2b6aa069d4fed6767d77c6ea30b0ada009
HeeNamgoong <skarndgml2@kookmin.ac.kr>
2024-08-18 14:58:22 +0900

    14503 - 8/18

--- 14503.py
+++ 14503.py
@@ -3,7 +3,7 @@ input = sys.stdin.readline
 
 N, M = map(int, input().split())
 
-r, c, d = map(int, input().split())
+r, c, d = map(int, input().split()) # 로봇의 위치 x,y / 방향 - 북:0, 동:1, 남:2, 서:3
 
 clean = []
 ans = 0
@@ -12,97 +12,51 @@ for i in range(N):
     clean.append(list(map(int, input().split())))
 
 while True:
-    # if (r == N-1 or c == M-1 or r == 0 or c == 0):
-    #     break
-    
-    if clean[r][c] == 0:
+
+    if clean[r][c] == 0: # 빈 칸
         ans += 1
-        clean[r][c] = 1
+        clean[r][c] = 2
+
+    if clean[r-1][c] != 0 and clean[r][c-1] != 0 and clean[r+1][c] != 0 and clean[r][c+1] != 0:
+        if d == 0: # 북
+            if clean[r+1][c] == 1:
+                break # 작동 멈춤
+            else:
+                r += 1
+        elif d == 2: # 남
+            if clean[r-1][c] == 1:
+                break
+            else:
+                r -= 1
+        elif d == 1: # 동
+            if clean[r][c-1] == 1:
+                break
+            else:
+                c -= 1
+        elif d == 3: # 서
+            if clean[r][c+1] == 1:
+                break
+            else:
+                c += 1
         continue
     
-    
-    elif (r != 0 and clean[r-1][c] == 1) and (c != 0 and clean[r][c-1] == 1) and (r != N-1 and clean[r+1][c] == 1) and (c != M-1 and clean[r][c+1] == 1):
-    #     pass
-    # elif (r != N-1 or c != M-1 or r != 0 or c != 0) and (clean[r-1][c] == 1 and clean[r][c-1] == 1 and clean[r+1][c] == 1 and clean[r][c+1] == 1):
-        
-        if d == 0 and r != N-1:
-            r += 1
-            continue
-        elif d == 1 and c != M-1:
-            c -= 1
-            continue
-        elif d == 2 and r != 0:
-            r -= 1
-            continue
-        elif d == 3 and c != 0:
-            c += 1
-            continue
-        else:
-            break 
-    # elif (clean[r-1][c] == 0 or clean[r][c-1] == 0 or clean[r+1][c] == 0 or clean[r][c+1] == 0):
-    elif (r != 0 and clean[r-1][c] == 0) or (c != 0 and clean[r][c-1] == 0) or (r != N-1 and clean[r+1][c] == 0) or (c != M-1 and clean[r][c+1] == 0):
-        if d == 0:
+    else: # 빈 칸이 있는 경우
+        if d == 0: # 북
             d = 3
             if clean[r][c-1] == 0:
                 c -= 1
-                continue
-        elif d == 1:
+        elif d == 1: # 동
             d = 0
             if clean[r-1][c] == 0:
-                r -= 1
-                continue
-        elif d == 2:
+                r -= 1 
+        elif d == 2: # 남
             d = 1
             if clean[r][c+1] == 0:
                 c += 1
-                continue
-        elif d == 3:
+        elif d == 3: # 서
             d = 2
             if clean[r+1][c] == 0:
                 r += 1
-                continue
-    else:
-        break
-         
+        continue # 1번으로 돌아감
+
 print(ans)
-
-
-while True:
-    if d == 0:
-        if r != N-1:
-            if (clean[r-1][c] == 1) and (clean[r][c-1] == 1) and (clean[r+1][c] == 1) and (clean[r][c+1] == 1):
-                r += 1
-                continue
-            else:
-                d = 3                    
-                if c == 0:
-                    break
-                elif clean[r][c-1] == 0:
-                    c -= 1
-                    continue
-        else:
-            if (clean[r-1][c] == 1) and (clean[r][c-1] == 1) and (clean[r][c+1] == 1):
-                break
-            else:
-                d = 3                    
-                if c == 0:
-                    break
-                elif clean[r][c-1] == 0:
-                    c -= 1
-                    continue
-                    
-    elif d == 1:
-        d = 0
-        if clean[r-1][c] == 0:
-            r -= 1
-            continue
-    elif d == 2:
-        d = 1
-        if clean[r][c+1] == 0:
-            c += 1
-            continue
-    elif d == 3:
-        d = 2
-        if clean[r+1][c] == 0:
-            r += 1
-            continue
